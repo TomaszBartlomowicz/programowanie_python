43beffbd8b17232326716070ff1b234284f943e8
TomaszBartlomowicz <bartlomowicz.tomasz@gmail.com>
2024-12-20 09:17:24 +0100

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -1,38 +1,29 @@
 import heapq
 
 def dijkstra(graph, start):
-
-    # Inicjalizacja odległości i kolejki priorytetowej
     distances = {node: float('inf') for node in graph}
     distances[start] = 0
     priority_queue = [(0, start)]
-
+    
     while priority_queue:
         current_distance, current_node = heapq.heappop(priority_queue)
 
-        # Pomijamy przetworzone wierzchołki
         if current_distance > distances[current_node]:
             continue
 
-        # Przeglądamy sąsiadów
         for neighbor, weight in graph[current_node].items():
             distance = current_distance + weight
 
-            # Aktualizacja dystansu, jeśli znaleziono krótszy
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
                 heapq.heappush(priority_queue, (distance, neighbor))
 
     return distances
 
-
 graph = {
     'A': {'B': 1, 'C': 4},
-    'B': {'C': 2, 'D': 6},
-    'C': {'D': 3},
-    'D': {}
+    'B': {'A': 1, 'C': 2, 'D': 6},
+    'C': {'A': 4, 'B': 2, 'D': 3},
+    'D': {'B': 6, 'C': 3}
 }
-
-# Startujemy z wierzchołka 'A'
-shortest_paths = dijkstra(graph, 'A')
-print(shortest_paths)
+print(dijkstra(graph, 'A'))
