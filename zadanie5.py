43bb3d460ba416aa53b9dca191be618b11b34e58
TomaszBartlomowicz <bartlomowicz.tomasz@gmail.com>
2024-12-20 09:15:52 +0100

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,61 +1,34 @@
 class State:
-    def __init__(self, name):
+    def __init__(self, name, output):
         self.name = name
+        self.output = output
         self.transitions = {}
 
     def add_transition(self, input_symbol, next_state):
-        """Dodaje przejście do innego stanu na podstawie symbolu wejściowego."""
         self.transitions[input_symbol] = next_state
 
     def next_state(self, input_symbol):
-        """Zwraca kolejny stan dla danego symbolu wejściowego."""
         return self.transitions.get(input_symbol, None)
 
 class MooreMachine:
-    def __init__(self):
-        self.states = {}
-        self.initial_state = None
-
-    def add_state(self, state, is_initial=False):
-        """Dodaje stan do automatu, opcjonalnie ustawiając go jako początkowy."""
-        self.states[state.name] = state
-        if is_initial:
-            self.initial_state = state
+    def __init__(self, states, initial_state):
+        self.states = states
+        self.current_state = initial_state
 
     def process(self, inputs):
-        """Przetwarza sekwencję symboli wejściowych."""
-        current_state = self.initial_state
         outputs = []
-
-        for input_symbol in inputs:
-            if current_state is None:
-                break
-            outputs.append(current_state.name)
-            current_state = current_state.next_state(input_symbol)
-
-        if current_state:
-            outputs.append(current_state.name)
-
+        for symbol in inputs:
+            outputs.append(self.current_state.output)
+            self.current_state = self.current_state.next_state(symbol)
+        outputs.append(self.current_state.output)
         return outputs
 
-# Przykład automatu Moore'a
-s1 = State("S1")
-s2 = State("S2")
-s3 = State("S3")
+
+s1 = State("S1", "0")
+s2 = State("S2", "1")
 
 s1.add_transition("a", s2)
-s1.add_transition("b", s1)
-s2.add_transition("a", s2)
-s2.add_transition("b", s3)
-s3.add_transition("a", s1)
-s3.add_transition("b", s3)
+s2.add_transition("b", s1)
 
-fsm = MooreMachine()
-fsm.add_state(s1, is_initial=True)
-fsm.add_state(s2)
-fsm.add_state(s3)
-
-# Przykładowe wejście
-data = ["a", "b", "a", "a", "b"]
-output = fsm.process(data)
-print(output)
+moore_machine = MooreMachine([s1, s2], s1)
+print(moore_machine.process(["a", "b", "a", "b"]))
